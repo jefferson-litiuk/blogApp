eeb86a078a94b80a921c3db4d74fb92532bc17dc
Jefferson Litiuk <jefferson.litiuk@hotmail.com>
2020-12-29 17:33:11 -0300

    Configurando body parser

---
+++ app.js
@@ -0,0 +1,21 @@
+
+//Iniciando módulos
+    const express = require('express');
+    const handlebars = require('express-handlebars');
+    const bodyParser = require('body-parser');
+    const app = express();
+    // const mongoose = require('mongoose');
+
+//Configurações
+    //body parser
+    app.use(bodyParser.urlencoded({extended:true}));
+    app.use(bodyParser.json());
+    
+
+//Rotas
+
+//Outros
+const PORT = 8081;
+app.listen(PORT,()=>{
+    console.log("Servidor Rodando!")
+});
