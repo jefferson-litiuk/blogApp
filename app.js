8674f47404fcffdb142c400ba643777693dbff6f
Jefferson Litiuk <jefferson.litiuk@hotmail.com>
2020-12-29 18:32:12 -0300

    Arquivos estáticos

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 
 //Iniciando módulos
-    const express = require('express');
-    const handlebars = require('express-handlebars');
-    const bodyParser = require('body-parser');
-    const app = express();
-    // const mongoose = require('mongoose');
+const express = require('express')
+const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const app = express()
+const admin = require('./routes/admin')
+const path = require('path')
+
+// const mongoose = require('mongoose');
 
 //Configurações
+
     //body parser
-    app.use(bodyParser.urlencoded({extended:true}));
-    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(bodyParser.json())
     // Handlebars 
-    app.engine('handlebars', handlebars({defaultLayout:'main'}));
-    app.set('view engine','handlebars');
+    app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+    app.set('view engine', 'handlebars')
+//Mongoose
+
+    //Public
+    app.use(express.static(path.join(__dirname,"public")))
+    
+
 //Rotas
+app.use('/admin', admin)
 
 //Outros
 const PORT = 8081;
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log("Servidor Rodando!")
-});
+})
