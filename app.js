62c6f2e4614faf3366c449234181adbfadbaffeb
Jefferson Litiuk <jefferson.litiuk@hotmail.com>
2021-01-13 10:34:06 -0300

    Iniciando registro de usuários

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ require("./models/Postagem")
 const Postagem = mongoose.model("postagens")
 require("./models/Categoria")
 const Categoria = mongoose.model("categorias")
+const usuarios = require('./routes/usuario')
 //Configurações
 
 //Sessão
@@ -117,9 +118,7 @@ app.get('/', (req, res) => {
 
 })
 app.use('/admin', admin)
-
-
-
+app.use('/usuarios', usuarios)
 
 //Outros
 const PORT = 8081;
